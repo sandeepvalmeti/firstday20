05ea65bb8535f58e19a38c285c88cc61ae74c708
sandeepvalmeti <sandeepvalmeti@gmail.com>
2019-12-20 15:31:47 +0530

    dayone

---
+++ src/org/emp/EmpName.java
@@ -0,0 +1,9 @@
+package org.emp;
+
+public class EmpName {
+	public void empname() {
+		System.out.println("sandeep");
+	}
+	
+
+}
---
+++ src/org/emp/Employee.java
@@ -0,0 +1,15 @@
+package org.emp;
+
+public class Employee {
+public void empId() {
+	System.out.println("223579");
+}
+public static void main(String[] args) {
+	Employee obj=new Employee();
+	//object creation syntax classname obj=new classname()//
+	obj.empId();
+	EmpName obj2=new EmpName();
+	obj2.empname();
+}
+}
+							
